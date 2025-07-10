f6df8c668cdd4531ac22f504cb8b21502608cd96
Adriano Alasia <adrianoalasia2020@gmail.com>
2025-07-10 10:21:25 -0400

    Can now connect and send packets

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 )
+
+func handle_sending(connection net.Conn) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println("Sent", scanner.Text())
+		connection.Write([]byte(scanner.Text() + "\r\n"))
+	}
+}
 
 func main() {
 	connection, err := net.Dial("tcp", "irc.dal.net:6667")
@@ -12,6 +22,8 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	go handle_sending(connection)
 
 	connection.Write([]byte("NICK Arpidanzo:\r\n"))
 	connection.Write([]byte("USER Arpidanzo 0 * :twint\r\n"))
