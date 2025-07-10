73747996c68fd157cfdb862daa6356c2d924a707
Adriano Alasia <adrianoalasia2020@gmail.com>
2025-07-10 16:36:01 -0400

    Finally a working client, but still lacking most IRC features to be called a full IRC server

--- main.go
+++ main.go
@@ -10,12 +10,39 @@ import (
 )
 
 const SERVER = "cloverse.duckdns.org:6667"
+const NICK = "AdriBot"
+const REAL_NAME = "Adriano"
 
-func handle_sending(message string, connection net.Conn) {
+var channels []string = []string{"#test"}
+var current_channel int = 0
+
+func send(message string, connection net.Conn) {
 	connection.Write([]byte(message + "\r\n"))
 }
 
-func parseMessage(message string) (string, string, []string) {
+func parseMessage(sender string, args []string) {
+}
+
+func log(message string) {
+	textView.SetText(textView.GetText(false) + message)
+	textView.ScrollToEnd()
+}
+
+func logCommand(_ string, command string, args []string) {
+	log(command + " [" + strings.Join(args, " ") + "]\n")
+}
+
+func logMessage(sender string, message string, self bool) {
+	var color string
+	if self {
+		color = "[blue]"
+	} else {
+		color = "[red]"
+	}
+	log(color + sender + ": [white]" + message + "\n")
+}
+
+func parseCommand(message string) (string, string, []string) {
 
 	var prefix string
 	var command string
@@ -48,10 +75,12 @@ func handle_reading(connection net.Conn) {
 			if []byte(msg)[0] == 0x00 {
 				continue
 			}
-			prefix, command, args := parseMessage(msg)
-			textView.SetText(textView.GetText(false) + prefix + " " + command + " [" + strings.Join(args, " ") + "]\n")
-			//fmt.Println(prefix, command, args)
-
+			prefix, command, args := parseCommand(msg)
+			if command == "PRIVMSG" {
+				logMessage(prefix, args[1], false)
+			} else {
+				logCommand(prefix, command, args)
+			}
 		}
 	}
 }
@@ -83,8 +112,8 @@ func main() {
 			return
 		}
 
-		textView.SetText(textView.GetText(false) + input.GetText() + "\n")
-		handle_sending(input.GetText(), connection)
+		logMessage("You", input.GetText(), true)
+		send("PRIVMSG "+channels[current_channel]+" :"+input.GetText(), connection)
 
 		input.SetText("")
 
@@ -95,9 +124,9 @@ func main() {
 
 	go handle_reading(connection)
 
-	connection.Write([]byte("NICK BotLol\r\n"))
-	connection.Write([]byte("USER BotLol 0 test :Arpidanzo \r\n"))
-	connection.Write([]byte("JOIN #test\r\n"))
+	send("NICK "+NICK, connection)
+	send("USER "+NICK+" 0 * :"+REAL_NAME, connection)
+	send("JOIN "+channels[0], connection)
 
 	if err := app.SetRoot(flex, true).Run(); err != nil {
 		panic(err)
